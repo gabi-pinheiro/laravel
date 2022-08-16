e3f315561e65d27397c2d695f6f5c88e7ffb3327
gabi-pinheiro <gabitigre2011@gmail.com>
2022-08-16 11:52:14 -0300

    subi o que temos do projeto até agora

---
+++ resources/views/contato.blade.php
@@ -0,0 +1,8 @@
+@extends('template.default')
+@section('content')
+
+EU AMO CONTATAR
+<img src="{{url('img/logo.jpg')}}" >
+
+
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+@extends('template.default')
+@section('content')
+
+Eu amo ninguém.
+
+@endsection
